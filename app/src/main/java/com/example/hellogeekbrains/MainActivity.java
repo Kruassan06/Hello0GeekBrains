0c7f60a32418352e59c32ca10928886616dab38d
Djama <spushkoy@yandex.ru>
2022-02-07 18:38:26 +0300

    Изменен фон, старый был банальный

--- app/src/main/java/com/example/hellogeekbrains/MainActivity.java
+++ app/src/main/java/com/example/hellogeekbrains/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.hellogeekbrains;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+private Button nextLayout;   // кнопки
+    private Button backLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nextLayout = findViewById(R.id.nextLayout);
+        backLayout = findViewById(R.id.back_Layout);
+
+nextLayout.setOnClickListener(this);
+
+
+  /*  nam(backLayout);
     }
+    static void nam (Button backLayout){
+        backLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.back_Layout:
+                        break;
+
+                }
+
+
+            }
+        });*/
+
+    }
+
+
+    @Override
+    public void onClick(View v) {   // процедура переключает между основным и второстепенным
+switch (v.getId()){
+    case R.id.nextLayout:
+        setContentView(R.layout.liner_calc);
+
+        break;
+
+
+
 }
+
+
+
+        }
+    }
